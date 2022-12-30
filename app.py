5a7f06c6b612e8b4a993d46c25f7413e5da7508f
Ilya Kaminsky <ilyakam@gmail.com>
2022-12-30 16:36:58 -0500

    chore(Dockerfile): switch to a custom image
    Point the Dockerfile to an image that was built manually from Cuda v11.3
    Fix the `inference()` definition to be a true Python function. And have
    it return an object indicating whether it's GPU-enabled or not.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import torch
+
 def init():
     print('Ran `init()` in `app.py`')
 
-def inference:
+def inference():
     print('Ran `inference()` in `app.py`')
+
+    has_cuda = torch.cuda.is_available()
+
+    return {"has_cuda": has_cuda}
