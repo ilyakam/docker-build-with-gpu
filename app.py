e1ce37aa0f5c9e3285e27e10a97a4dae271bedc8
Ilya Kaminsky <ilyakam@gmail.com>
2022-12-31 18:17:27 -0500

    fix(app): provide the missing parameter to `inference()`
    This fix resolves the following error message:
    > TypeError: inference() takes 0 positional arguments but 1 was given
    
    According to these docs[1], `inference()` receives the `model_inputs`
    parameter from the SDK.
    
    [1] https://docs.banana.dev/banana-docs/core-concepts/inference-server/
        serverless-framework/app.py#inference

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import torch
 def init():
     print('Ran `init()` in `app.py`')
 
-def inference():
-    print('Ran `inference()` in `app.py`')
+def inference(model_inputs:dict) -> dict:
+    print(f'Ran `inference()` in `app.py` with a dict of {model_inputs}')
 
     has_cuda = torch.cuda.is_available()
 
